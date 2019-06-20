83e740075b948a6e1cb8da5ed5d8897392e6ac75
bflorea <bflorea@deloittece.com>
2019-06-20 12:57:04 +0300

    hide menu items

--- frontend/todo/src/app/service/hardcoded-authentication.service.ts
+++ frontend/todo/src/app/service/hardcoded-authentication.service.ts
@@ -5,11 +5,18 @@ import { Injectable } from '@angular/core';
 })
 export class HardcodedAuthenticationService {
 
-  constructor() {}
-  
-  authenticate(username, password){
+  constructor() { }
+
+  authenticate(username, password) {
     if (username === "negura" && password === "noaptea") {
+      sessionStorage.setItem('authenticatedUser', username);
       return true;
     }
+    return false;
+  }
+
+  isUserLogedIn() {
+    let user = sessionStorage.getItem('authenticatedUser');
+    return !(user === null);
   }
 }
